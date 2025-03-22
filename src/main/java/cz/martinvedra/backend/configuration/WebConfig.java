9c81e13613f25861315bd28000cfa562798b9c8b
Martin Ve <martinvetsu@gmail.com>
2025-03-22 18:06:12 +0100

    db settings

--- src/main/java/cz/martinvedra/backend/configuration/WebConfig.java
+++ src/main/java/cz/martinvedra/backend/configuration/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints under /api
                 .allowedOrigins("http://localhost:3000") // Or your specific origins
+                .allowedOrigins("https://martinvedra.cz") // Or your specific origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Or your specific methods
                 .allowedHeaders("Authorization", "Content-Type"); // Specifikujeme hlavičky
 //                .allowedHeaders("*"); // Or your specific headers (better practice)
